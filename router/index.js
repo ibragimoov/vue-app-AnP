0666c9be6871b3cb3d6d5af3184474da4e237532
Eldar <1ibragimov.e.i20@gmail.com>
2021-10-22 00:36:15 +0300

    commit #12 error correction

--- router/index.js
+++ router/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import Ad from '@/components/Ads/Ad'
-import AdList from '@/components/Ads/AdList'
-import NewAd from '@/components/Ads/NewAd'
-import Login from '@/components/Auth/Login'
-import Registration from '@/components/Auth/Registration'
-import Orders from '@/components/User/Orders'
+
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Ad from '@/components/Ads/Ad';
+import AdList from '@/components/Ads/AdList';
+import NewAd from '@/components/Ads/NewAd';
+import Login from '@/components/Auth/Login';
+import Registration from '@/components/Auth/Registration';
+import Orders from '@/components/User/Orders';
 
 export default new Router({
 	routes: [
