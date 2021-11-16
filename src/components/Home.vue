dd20e6ca1ed2ac9ad3b6c45c36a9114ab4b479df
Eldar <1ibragimov.e.i20@gmail.com>
2021-11-17 00:00:42 +0300

    commit #23 connecting date to carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,21 @@
 		<v-layout row> 
 			<v-flex xs12>
 				<v-carousel>
-					<v-carousel-item></v-carousel-item>
+
+					<v-carousel-item
+                    v-for="ad in ads"
+                    :key="ad.id"
+                    :src="ad.src"
+                >		
+                >
+                    <div class="ad-link">
+                        <v-btn class="error" :to="'/ad/' + ad.id">
+                            {{ ad.title }}
+                        </v-btn>
+                    </div>
+
+                </v-carousel-item>
+
 				</v-carousel>
 			</v-flex>
 		</v-layout> 
@@ -38,6 +52,18 @@ class="headline mb0"
 	</div>
 </template>
 
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 15px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
 
 
 <script>
