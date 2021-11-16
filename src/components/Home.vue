e7b14bd09a44316c68054e07045b1d1c7afd6fc8
Eldar <1ibragimov.e.i20@gmail.com>
2021-11-17 00:16:42 +0300

    commit #24 connecting grid(missing btn)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,47 +4,50 @@
 		<v-layout row> 
 			<v-flex xs12>
 				<v-carousel>
-
 					<v-carousel-item
-                    v-for="ad in ads"
-                    :key="ad.id"
-                    :src="ad.src"
-                >		
-                >
+					v-for="ad in ads"
+					:key="ad.id"
+					:src="ad.src"
+					>
                     <div class="ad-link">
-                        <v-btn class="error" :to="'/ad/' + ad.id">
-                            {{ ad.title }}
-                        </v-btn>
+							<v-btn 
+                            class= "error" 
+                            :to="'/ad/' + ad.id"> 
+                                {{ad.title}}
+							</v-btn>
                     </div>
-
-                </v-carousel-item>
-
+					</v-carousel-item>
 				</v-carousel>
 			</v-flex>
 		</v-layout> 
 	</v-container>
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
+	<v-container grid-list-lg>
+		<v-layout row wrap> 
+			<v-flex xs12 sm6 md4
+			v-for="ad in ads"
+			:key="ad.id"
+			>
 				<v-card>
 					<v-img
+					:src="ad.src"
 					height="200px"
 					></v-img>
 					<v-card-title primary-title>
-					<div>
-						<h3 
-class="headline mb0"
->Здесь будет заголовок</h3>
-						<div>Здесь будет описание</div>
-					</div>
+						<div>
+						<h3 class="headline mb-0">{{ad.title}}</h3>
+						<div>{{ad.desc}}</div>
+						</div>
 					</v-card-title>
 					<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn text>Open</v-btn>
+					<v-btn text :to="'/ad/' + ad.id">
+						Open
+					</v-btn>
 					<v-btn raised color="primary">
 						Buy
 					</v-btn>
-					</v-card-actions>				
+				</v-card-actions>	
+					
 				</v-card>
 			</v-flex>
 		</v-layout> 
@@ -52,25 +55,11 @@ class="headline mb0"
 	</div>
 </template>
 
-<style scoped>
-	.ad-link {
-		position:absolute;
-		bottom: 50px;
-		left:50%;
-		background: rgb(0,0,0,0.5);
-		transform: translate(-50%,0);
-		padding: 5px 15px;
-		border-top-right-radius: 5px;
-		border-top-left-radius: 5px;
-	}
-</style>
-
-
 <script>
 export default {
-    data () { 
-	return {
-		ads:[
+	data () { 
+		return {
+			ads:[
 			{
 				title:"First",
 				desc:"First Desc",
@@ -99,8 +88,22 @@ export default {
 				src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
 				id:"4"
 			}
-		]
-	} 	
-  }
-}
+
+			]
+		} 	
+	}
+} 
 </script>
+
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		right:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 15px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
