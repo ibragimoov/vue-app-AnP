05f0c4cca08d2f1f75e4c13dd38a30c1e41bbe6d
Eldar <1ibragimov.e.i20@gmail.com>
2021-11-16 22:56:06 +0300

    commit #15 variable for input fields

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -10,15 +10,17 @@
 					<v-form>
 					<v-text-field			          
                         prepend-icon="mdi-account" 	
-						name="email"              			
-                        label="Email" 
-                        type="email">
+						name = "email"              			
+                        label = "Email" 
+                        type = "email"
+                        v-model = "email">
 					</v-text-field>
 					<v-text-field
                        prepend-icon="mdi-lock" 
-                        name="password" 
-                        label="Password" 
-                        type="password">
+                        name = "password" 
+                        label = "Password" 
+                        type = "password"
+                        v-model = "password">
                     </v-text-field>
 					</v-form> 
 				</v-card-text>
@@ -35,7 +37,9 @@
 export default {
 	data () { 
 		return {
-		} 	
+            email: "",
+            password: ""
+		}
 	}
-} 
+}
 </script>
