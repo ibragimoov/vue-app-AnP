fe1040e92a331a701c5d032e72b4e8ad80f8e7b5
Eldar <1ibragimov.e.i20@gmail.com>
2021-12-15 08:56:06 +0300

    1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import router from '../router/index';
+import router from './router/index';
 import store from './store';
 import fb from 'firebase';
 
