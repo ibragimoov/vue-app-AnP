7336106663f153c23d7d815bc84455c200fb2415
Eldar <1ibragimov.e.i20@gmail.com>
2022-01-24 20:53:26 +0300

    17_2

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -2,6 +2,7 @@
 	<v-container>
 		<v-layout row> 
 			<v-flex xs12 sm6 offset-sm3>
+
 				<h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
 
 				<v-form v-model="valid" ref="form" validation>
@@ -23,48 +24,50 @@
 					class="mb-3" 
 					></v-textarea>
 
-				</v-form>
+					</v-form>
 
-				<v-layout  row>
-					<v-flex xs12>
-					<v-btn 
-					class="mt-3"
-					color="warning">
-					Upload<v-icon right dark>mdi-cloud-upload</v-icon>
-					</v-btn>
-					</v-flex>
-				</v-layout>
+					<v-layout  row>
+						<v-flex xs12>
+						<v-btn 
+						class="mt-3"
+						color="warning">
+						Upload
+						<v-icon right dark>mdi-cloud-upload</v-icon>
+						</v-btn>
+						</v-flex>
+					</v-layout>
 
-				<v-layout row>
-					<v-flex xs12>
-					<img 	src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
-					</v-flex>
-				</v-layout>
+					<v-layout row>
+						<v-flex xs12>
+						<img 		src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="150" class="mt-3">
+						</v-flex>
+					</v-layout>
 
-				<v-layout row>
-					<v-flex xs12>
-					<v-switch
-					v-model="promo"
-					label="Ad to Promo?"
+					<v-layout row>
+						<v-flex xs12>
+						<v-switch
+						v-model="promo"
+						label="Ad to Promo?"
 						></v-switch>
-					</v-flex>
-				</v-layout>
+						</v-flex>
+					</v-layout>
 
-				<v-layout row>
-					<v-flex xs12>
-					<v-spacer></v-spacer>
-					<v-btn 
-					color="success"
-					@click="createAd" 
-					>Create Ad</v-btn>
-					</v-flex>
-				</v-layout>
-				
+					<v-layout row>
+						<v-flex xs12>
+						<v-spacer></v-spacer>
+						<v-btn 
+						color="success"
+						@click="createAd"
+						:loading="loading"
+						:disabled:="!valid || loading" 
+						>Create Ad</v-btn>
+						</v-flex>
+					</v-layout>
+					
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
-
 <script>
 export default {
 	data () { 
@@ -77,18 +80,30 @@ export default {
 	},
 	methods: {
 		createAd(){
-			if (this.$refs.form.validate())
-			{
-				const ad = {
-					title: this.title,
-					desc: this.description,
-					promo: this.promo,
-					src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
-				}
-				
-			this.$store.dispatch("createAd", ad)
+			if (this.$refs.form.validate()){
+			const ad = {
+				title: this.title,
+				desc: this.description,
+				promo: this.promo,
+				src: "https://www.ginx.tv/uploads2/Various/Brawl_Stars/Brawl_Stars_cover.jpg?ezimgfmt=ng%3Awebp%2Fngcb5%2Frs%3Adevice%2Frscb5-1"
 			}
-		}
+			this.$store.dispatch("createAd", ad)
+			.then(() => {
+			this.$router.push("/list")
+			})
+			.catch(() => {})
+
+
+			}
+		},
+		computed: {
+			loading() {
+			return this.$store.getters.loading
 	}
+},
+
+	}
+
+
 } 
 </script>
